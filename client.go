75b558961a2e28a8b5f4a848692ddd1a1f23b6fa
Sanjay Bharadhwaj Vijayaraghava <sanjaybwaj@gmail.com>
2017-03-19 17:44:31 -0400

    Add Client struct
    First, multiple writes were happening which created a panic and stopped
    the application. Writes on the websocket connection had to be
    serialized.
    
    So a new client struct is written that has its own write worker that
    does the same. On a side note, it created a cleaner code.

---
+++ client.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	ws "github.com/gorilla/websocket"
+	uuid "github.com/satori/go.uuid"
+	"github.com/streamrail/concurrent-map"
+)
+
+var connPool cmap.ConcurrentMap
+
+// Client represents a client
+type Client struct {
+	conn   *ws.Conn
+	connID string
+
+	writeChan chan []byte
+}
+
+// NewClient creates a new client object with the given websocket connection
+func NewClient(c *ws.Conn) *Client {
+	client := &Client{
+		conn:      c,
+		connID:    uuid.NewV4().String(),
+		writeChan: make(chan []byte),
+	}
+	go client.pingWorker()
+	go client.writeWorker()
+	go client.broadcastWorker()
+
+	log.Println("client", "new", client.connID, client.conn.RemoteAddr())
+	connPool.Set(client.connID, client)
+
+	return client
+}
+
+// Close closes the underlying websocket connection
+func (c *Client) Close(from string) {
+
+	connPool.Remove(c.connID)
+	c.conn.Close()
+	log.Println("client", "exit-from", from, c.connID)
+}
+
+func (c *Client) pingWorker() {
+
+	for {
+		if err := c.conn.WriteControl(
+			ws.PingMessage, nil, time.Now().Add(5*time.Second)); err != nil {
+			log.Println(err)
+			break
+		}
+		time.Sleep(5 * time.Second)
+	}
+
+	c.Close("ping")
+}
+
+func (c *Client) writeWorker() {
+	for {
+		data := <-c.writeChan
+		if err := c.conn.WriteMessage(ws.TextMessage, data); err != nil {
+			log.Println(err)
+			break
+		}
+	}
+
+	c.Close("write")
+}
+
+func (c *Client) broadcastWorker() {
+	for {
+		// read
+		_, data, err := c.conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			break
+		}
+
+		// broadcast
+		for connTuple := range connPool.IterBuffered() {
+			connTuple.Val.(*Client).writeChan <- data
+		}
+	}
+
+	c.Close("broadcast")
+}
--- main.go
+++ main.go
@@ -5,23 +5,18 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	ws "github.com/gorilla/websocket"
-	"github.com/satori/go.uuid"
 	"github.com/streamrail/concurrent-map"
 )
 
 const defaultPort = "8080"
 
-// upgrader is used to upgrade HTTP connections to Websocket
+// upgrades HTTP connections to Websocket
 var upgrader = ws.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
-
-var broadcast chan []byte
-var connPool cmap.ConcurrentMap
 
 func main() {
 
@@ -36,8 +31,6 @@ func main() {
 	http.HandleFunc("/ws", handleWs)
 
 	connPool = cmap.New()
-	broadcast = make(chan []byte)
-	go broadcastWorker(broadcast)
 
 	log.Println("starting server on :" + port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
@@ -54,59 +47,7 @@ func handleWs(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	connID := uuid.NewV4().String()
-	connPool.Set(connID, conn)
-	defer connPool.Remove(connID)
-
-	log.Println("client", "new", connID, r.RemoteAddr)
-
-	// PingWorker send pings to keep the connection alive
-	go func() {
-		for {
-			if _, ok := connPool.Get(connID); ok {
-				if err := conn.WriteControl(
-					ws.PingMessage, nil, time.Now().Add(5*time.Second)); err != nil {
-					log.Println(err)
-				}
-				time.Sleep(5 * time.Second)
-			} else {
-				break
-			}
-		}
-	}()
-
-	// read messages and send it to broadcast channel
-	for {
-		_, data, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			break
-		}
-		broadcast <- data
-	}
-
-	conn.Close()
-	log.Println("client", "exit", connID)
-}
-
-// BroadcastWorker sends the message to all connections in connPool
-// in its own goroutine.
-func broadcastWorker(broadcast chan []byte) {
-	log.Println("broadcastWorker running")
-
-	for {
-		// get the data
-		data := <-broadcast
-
-		// broadcast the data
-		for connTuple := range connPool.IterBuffered() {
-			go func(conn *ws.Conn, data []byte) {
-				if err := conn.WriteMessage(ws.TextMessage, data); err != nil {
-					log.Println(err)
-				}
-			}(connTuple.Val.(*ws.Conn), data)
-		}
-	}
+	NewClient(conn)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
